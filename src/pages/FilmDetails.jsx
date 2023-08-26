36985eea357606296c400a004f946edbb7f8a4e3
Tetiana Babak <712tanech@gmail.com>
2023-08-26 19:27:19 +0100

    add header

--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -3,7 +3,10 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "../components/Header/Header";
 import { Grid } from "@mui/material";
+<<<<<<< HEAD
 import Tags from "../components/Tags/Tags";
+=======
+>>>>>>> f4e99e1 (add header)
 
 function FilmDetails() {
     const {filmId} = useParams();
@@ -25,7 +28,11 @@ function FilmDetails() {
     const { name, rating, genres, language, premiered, image, summary, runtime} = filmData;
 
     return (
+<<<<<<< HEAD
      
+=======
+     <>
+>>>>>>> f4e99e1 (add header)
      <Grid container>
         <Header 
         name={name}
@@ -37,10 +44,15 @@ function FilmDetails() {
         summary={summary}
         runtime={runtime}
         />
+<<<<<<< HEAD
         <Tags genres={genres}/>
         </Grid>
         
      
+=======
+        </Grid>
+     </>
+>>>>>>> f4e99e1 (add header)
     )
 }
 
